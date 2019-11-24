30fd6b5dafcf2d4abfb54b3dbb1c38735e97900a
Ferdinand <ferdinand.obermeier@gmail.com>
2019-11-24 16:45:00 +0100

    Filter, Color changes
    Added filter and changed app color

--- tourcast/frontend/tourcast-app/src/app/nav-bar/nav-bar.component.ts
+++ tourcast/frontend/tourcast-app/src/app/nav-bar/nav-bar.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  sunny = true;
+  cloudy = false;
+  showFilter = false;
+
   constructor() { }
 
-  sunny:boolean=true;
-  cloudy:boolean=false;
   ngOnInit() {
   }
 
