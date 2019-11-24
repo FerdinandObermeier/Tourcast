df9018669a5f839a978bd70399af6932d26dd8a0
Andreas <ga96mux@mytum.de>
2019-11-24 15:34:32 +0100

    Swiper first setup

--- tourcast/frontend/tourcast-app/src/app/main-page/main-page.component.ts
+++ tourcast/frontend/tourcast-app/src/app/main-page/main-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements AfterViewInit {
+
+  mySwiper: Swiper;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  // this.mySwiper = new Swiper('.swiper-container', {
+  //       pagination: {
+  //         el: '.swiper-pagination',
+  //         dynamicBullets: true,
+  //       },
+  //     });
+
+      ngAfterViewInit() {
+        this.mySwiper = new Swiper('.swiper-container', {
+            // pagination: '.swiper-pagination',
+            // paginationClickable: true,
+            pagination: {
+              el: '.swiper-pagination',
+              dynamicBullets: true,
+            },
+            // nextButton: '.swiper-button-next',
+            // prevButton: '.swiper-button-prev'
+        });
+      }
+  
 
 }
