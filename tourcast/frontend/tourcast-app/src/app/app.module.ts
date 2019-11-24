06f779d905f7b6d71755a8d88f588708bcc047e3
Andreas <ga96mux@mytum.de>
2019-11-24 12:32:08 +0100

    Added components
    detail-view, main-page, nav-bar, filter

--- tourcast/frontend/tourcast-app/src/app/app.module.ts
+++ tourcast/frontend/tourcast-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainPageComponent,
+    DetailViewComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
