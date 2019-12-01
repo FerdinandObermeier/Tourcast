9bbf5e2e286dc9c028d5703133b7a94e70a718e5
Andreas <ga96mux@mytum.de>
2019-12-01 22:47:29 +0100

    Working filter through filters service based on categories

--- tourcast/frontend/tourcast-app/src/app/card/card.component.ts
+++ tourcast/frontend/tourcast-app/src/app/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterService } from '../services/filter.service';
+import { Filters } from '../filters.enum';
 
 @Component({
   selector: 'app-card',
@@ -9,15 +11,44 @@ export class CardComponent implements OnInit {
 
   showDetails: boolean = false;
 
-  constructor() { }
+  constructor(
+    private filterService: FilterService
+  ) { }
 
-  local_activity:boolean=true;
-  directions_boat:boolean=false;
-  landscape:boolean=false;
-  museum:boolean=false;
-  panorama:boolean=false;
+  // local_activity:boolean=true;
+  // directions_boat:boolean=false;
+  // landscape:boolean=false;
+  // museum:boolean=false;
+  // panorama:boolean=false;
+
+  showAttractions = true;
+  showLakes = true;
+  showMountains = true;
+  showMuseums = true;
+  showViewpoints = true;
 
   ngOnInit() {
+    this.filterService.filterChange.subscribe(({filterType, value}) => {
+      switch (filterType) {
+        case Filters.Attractions:
+          this.showAttractions = value;
+          break;
+        case Filters.Lakes:
+            this.showLakes = value;
+          break;
+        case Filters.Mountains:
+            this.showMountains = value;
+          break;
+        case Filters.Museums:
+          this.showMuseums = value;
+          break;
+        case Filters.Viewpoints:
+          this.showViewpoints = value;
+          break;
+        default:
+          break;
+      }
+    });
   }
 
   onCloseDetails(showDetails: boolean) {
