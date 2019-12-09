cb1a94586dddb75efc636542ecf86c63535e34b1
Andreas Kreitmair <ga96mux@mytum.de>
2019-12-09 20:25:11 +0100

    Added backend connection for time rating and price

--- tourcast/frontend/tourcast-app/src/app/detail-view/detail-view.component.ts
+++ tourcast/frontend/tourcast-app/src/app/detail-view/detail-view.component.ts
@@ -9,10 +9,25 @@ export class DetailViewComponent implements OnInit {
 
   @Output() onCloseDetails = new EventEmitter<boolean>();
   @Input() cardInfo;
+  @Input() timeFrom;
+  @Input() timeTo;
+
+  priceTag: any;
+  openingTime: any;
+  rating: any;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.cardInfo);
+    if (this.cardInfo.priceMax == '0.00') {
+      this.priceTag = 'Free Entrance';
+    } else {
+      this.priceTag = this.cardInfo.priceMin.split('.') + ' - ' + this.cardInfo.priceMax.split('.') + ' €';
+    }
+    this.openingTime = this.timeFrom + ' - ' + this.timeTo; 
+    this.rating = +this.cardInfo.rating.slice(0,1);
+    console.log(this.rating);
   }
 
   onClose() {
