c88f81ec5ed42b66858bc2bc097c67ed26707c72
Andreas Kreitmair <ga96mux@mytum.de>
2019-12-09 20:40:52 +0100

    Open All day

--- tourcast/frontend/tourcast-app/src/app/detail-view/detail-view.component.ts
+++ tourcast/frontend/tourcast-app/src/app/detail-view/detail-view.component.ts
@@ -20,12 +20,18 @@ export class DetailViewComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.cardInfo);
+    console.log(this.timeFrom);
+    console.log(this.timeTo);
     if (this.cardInfo.priceMax == '0.00') {
       this.priceTag = 'Free Entrance';
     } else {
       this.priceTag = this.cardInfo.priceMin.split('.') + ' - ' + this.cardInfo.priceMax.split('.') + ' €';
     }
-    this.openingTime = this.timeFrom + ' - ' + this.timeTo; 
+    if (this.timeFrom == '00:00' && this.timeTo == '23:59') {
+      this.openingTime = 'All Day';
+    } else {
+      this.openingTime = this.timeFrom + ' - ' + this.timeTo;
+    }
     this.rating = +this.cardInfo.rating.slice(0,1);
     console.log(this.rating);
   }
