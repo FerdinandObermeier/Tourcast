16f69514c18bf097c7aa3970b26ccb000d47bf02
Ferdi <ga65qak@mytum.de>
2020-01-27 23:43:45 +0100

    style changes. Added "I will go there" functionality

--- tourcast/frontend/tourcast-app/src/app/detail-view/detail-view.component.ts
+++ tourcast/frontend/tourcast-app/src/app/detail-view/detail-view.component.ts
@@ -19,6 +19,8 @@ export class DetailViewComponent {
   openingTime: any;
   rating: any;
   currentTime;
+  numberOfVisitors = 1;
+  alreadyVoted = false;
 
   constructor(private messageService: MessageService, private backendService: BackendService) {
     this.messageService.getShowDetails().subscribe((data: any) => {
@@ -26,6 +28,12 @@ export class DetailViewComponent {
       this.onCalculateInformation();
       this.showDetails = true;
       this.closeDetail = false;
+      this.numberOfVisitors = parseInt(this.cardInfo.crowdedness0) + parseInt(this.cardInfo.crowdedness2) + parseInt(this.cardInfo.crowdedness4) + parseInt(this.cardInfo.crowdedness6)
+      + parseInt(this.cardInfo.crowdedness8) + parseInt(this.cardInfo.crowdedness10) + parseInt(this.cardInfo.crowdedness12) + parseInt(this.cardInfo.crowdedness14) + parseInt(this.cardInfo.crowdedness16)
+      + parseInt(this.cardInfo.crowdedness18) + parseInt(this.cardInfo.crowdedness20) + parseInt(this.cardInfo.crowdedness22);
+     this.numberOfVisitors = this.numberOfVisitors !== 0 ? this.numberOfVisitors : 1;
+     console.log(this.numberOfVisitors);
+     console.log(this.cardInfo);
     });
 
     this.currentTime = new Date().getHours();
@@ -54,11 +62,74 @@ export class DetailViewComponent {
   }
 
   onGoThere() {
-    const formData = this.cardInfo;
-    if (this.currentTime >= 0 && this.currentTime < 2) {
-      formData.crowdedness0 = formData.crowdedness0 + 1;
+    if (this.alreadyVoted) {
+      return;
+    } else {
+      this.alreadyVoted = true;
+      let key;
+      let value;
+      if (this.currentTime >= 0 && this.currentTime < 2) {
+        this.cardInfo.crowdedness0++;
+        key = 'crowdedness0';
+        value = this.cardInfo.crowdedness0;
+      }
+      if (this.currentTime >= 2 && this.currentTime < 4) {
+        this.cardInfo.crowdedness2++;
+        key = 'crowdedness2';
+        value = this.cardInfo.crowdedness2;
+      }
+      if (this.currentTime >= 4 && this.currentTime < 6) {
+        this.cardInfo.crowdedness4++;
+        key = 'crowdedness4';
+        value = this.cardInfo.crowdedness4;
+      }
+      if (this.currentTime >= 6 && this.currentTime < 8) {
+        this.cardInfo.crowdedness6++;
+        key = 'crowdedness6';
+        value = this.cardInfo.crowdedness6;
+      }
+      if (this.currentTime >= 8 && this.currentTime < 10) {
+        this.cardInfo.crowdedness8++;
+        key = 'crowdedness8';
+        value = this.cardInfo.crowdedness8;
+      }
+      if (this.currentTime >= 10 && this.currentTime < 12) {
+        this.cardInfo.crowdedness10++;
+        key = 'crowdedness10';
+        value = this.cardInfo.crowdedness10;
+      }
+      if (this.currentTime >= 12 && this.currentTime < 14) {
+        this.cardInfo.crowdedness12++;
+        key = 'crowdedness12';
+        value = this.cardInfo.crowdedness12;
+      }
+      if (this.currentTime >= 14 && this.currentTime < 16) {
+        this.cardInfo.crowdedness14++;
+        key = 'crowdedness14';
+        value = this.cardInfo.crowdedness14;
+      }
+      if (this.currentTime >= 16 && this.currentTime < 18) {
+        this.cardInfo.crowdedness16++;
+        key = 'crowdedness16';
+        value = this.cardInfo.crowdedness16;
+      }
+      if (this.currentTime >= 18 && this.currentTime < 20) {
+        this.cardInfo.crowdedness18++;
+        key = 'crowdedness18';
+        value = this.cardInfo.crowdedness18;
+      }
+      if (this.currentTime >= 20 && this.currentTime < 22) {
+        this.cardInfo.crowdedness20++;
+        key = 'crowdedness20';
+        value = this.cardInfo.crowdedness20;
+      }
+      if (this.currentTime >= 22 && this.currentTime < 24) {
+        this.cardInfo.crowdedness22++;
+        key = 'crowdedness22';
+        value = this.cardInfo.crowdedness22;
+      }
+      this.backendService.post('cards/', this.cardInfo.id, key, value);
     }
-    this.backendService.post('cards/', this.cardInfo);
   }
 
   onOpenLink(link: string) {
--- tourcast/frontend/tourcast-app/src/app/services/http.service.ts
+++ tourcast/frontend/tourcast-app/src/app/services/http.service.ts
@@ -15,38 +15,10 @@ export class BackendService {
     return this.httpClient.get(this.baseUrl + url).toPromise().catch(err => console.log(err));
   }
 
-  public post(url, data) {
+  public post(url, id, key, value) {
     const formData = new FormData();
-    console.log(data.name);
-    formData.set('name', data.name);
-    formData.set('attraction', data.attraction);
-    formData.set('lake', data.lake);
-    formData.set('mountain', data.mountain);
-    formData.set('museum', data.museum);
-    formData.set('viewpoint', data.viewpoint);
-    formData.set('img', data.img);
-    formData.set('crowdedness0', data.crowdedness0);
-    formData.set('crowdedness2', data.crowdedness2);
-    formData.set('crowdedness4', data.crowdedness4);
-    formData.set('crowdedness6', data.crowdedness6);
-    formData.set('crowdedness8', data.crowdedness8);
-    formData.set('crowdedness10', data.crowdedness10);
-    formData.set('crowdedness12', data.crowdedness12);
-    formData.set('crowdedness14', data.crowdedness14);
-    formData.set('crowdedness16', data.crowdedness16);
-    formData.set('crowdedness18', data.crowdedness18);
-    formData.set('crowdedness20', data.crowdedness20);
-    formData.set('crowdedness22', data.crowdedness22);
-    formData.set('rating', data.rating);
-    formData.set('priceMin', data.priceMin);
-    formData.set('priceMax', data.priceMax);
-    formData.set('mapsURL', data.mapsURL);
-    formData.set('openingHoursFrom', data.openingHoursFrom);
-    formData.set('openingHoursTo', data.openingHoursTo);
-    formData.set('description', data.description);
-    // formData.set('weather', data.weatherIndex);
-    formData.set('weather', '10');
+    formData.set(key, value);
     console.log(formData);
-    return this.httpClient.post(this.baseUrl + url, formData).toPromise().catch(err => console.log(err));
+    return this.httpClient.patch(this.baseUrl + url + id + '/', formData).toPromise().catch(err => console.log(err));
   }
 }
